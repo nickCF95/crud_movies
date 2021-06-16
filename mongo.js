e00fc2d5dd4e29dae1797a5dc755a0710f7fae16
nickCF95 <nicolascfernandez95@gmail.com>
2021-06-16 08:24:49 -0500

    refactor connection to database

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGO_DB_URI
+const connectionString = process.env.MONGODB_URI
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
